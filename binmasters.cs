38b208a6582d145082f815511dc0a32d0ef11d08
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-10-17 19:15:05 -0600

    Update binmasters.cs

--- binmasters.cs
+++ binmasters.cs
@@ -11,7 +11,7 @@ http://binmasters.tk/
 Copyright 2022 Bin Studios
 Support@binmasters.tk
 */
-public static class binmasters {
+public static class app {
     public static T RandomElement<T>(this IEnumerable<T> coll) {
         var rnd = new Random();
         return coll.ElementAt(rnd.Next(coll.Count()));
@@ -71,6 +71,8 @@ public static class binmasters {
             Console.WriteLine("1 Bin = $" + price);
             Console.WriteLine("Binmasters Profile: " + name);
 
+            //start shop
+        
             //bin updating / main loop
             while(x <= 4) {
                 
@@ -92,10 +94,143 @@ public static class binmasters {
                     bins = addbins;
                     goto Main;
                 }
+
+                int price10 = price * 10;
+                int price50 = price * 50;
+                
+                if(money == price10 ) {
+                    Console.Clear();
+                    Console.WriteLine("---SHOP---");
+                    Console.WriteLine("1. || Buy more Bins Per Second (BPS)");
+                    Console.WriteLine("2. || Trade bins for money");
+                    Console.WriteLine("3. || Back");
+
+                    string shopInput = Console.ReadLine();
+                    
+                    if(shopInput == "1") {
+                        int bps_can_buy = money / price / 2;
+                        int bps_can_buy_price = bps_can_buy * price;
+                        Console.WriteLine(bps_can_buy);
+                        Console.WriteLine("Would you like to buy " + bps_can_buy + " Bins per Second at: $" + bps_can_buy_price + " each?: (y/n)");
+                        string bps_buy = Console.ReadLine();
+                        
+                        if(bps_buy == "y") {
+                            Console.WriteLine("Bought " + bps_can_buy);
+                            int newestbps = bins_per_second + bps_can_buy;
+                            bins_per_second = newestbps;
+                            int newestmoney = money - bps_can_buy_price;
+                            money = newestmoney;
+                            Console.Clear();
+                            continue;
+                        }
+
+                        if(bps_buy == "n") {
+                            Console.Clear();
+                            continue;
+                        }
+
+                        else {
+                            Console.WriteLine("Incorrect answer, use (y/n)");
+                            Thread.Sleep(570);
+                            Console.Clear();
+                            continue;
+                        }
+                    }
+
+                    if(shopInput == "2") {
+                        int amount = bins * price;
+                        Console.WriteLine("Are you sure you would like to trade your bins? (y/n)");
+                        string tradeInput = Console.ReadLine();
+
+                        if(tradeInput == "y") {
+                            int addedmoneyv3 = amount + money;
+                            money = addedmoneyv3;
+                            continue;
+                        }
+
+                        if(tradeInput == "n") {
+                            continue;
+                        }
+
+                        else {
+                            continue;
+                        }
+                    }
+
+                    if(shopInput == "3") {
+                        continue;
+                    }
+                }
+
+                if(money == price50) {
+                    Console.Clear();
+                    Console.WriteLine("---SHOP---");
+                    Console.WriteLine("1. || Buy more Bins Per Second (BPS)");
+                    Console.WriteLine("2. || Trade bins for money");
+                    Console.WriteLine("3. || Back");
+
+                    string shopInput1 = Console.ReadLine();
+                    
+                    if(shopInput1 == "1") {
+                        int bps_can_buy = money / price / 2;
+                        int bps_can_buy_price = bps_can_buy * price;
+                        Console.WriteLine(bps_can_buy);
+                        Console.WriteLine("Would you like to buy " + bps_can_buy + " Bins per Second at: $" + bps_can_buy_price + " each?: (y/n)");
+                        string bps_buy1 = Console.ReadLine();
+                        
+                        if(bps_buy1 == "y") {
+                            Console.WriteLine("Bought " + bps_can_buy);
+                            int newestbps = bins_per_second + bps_can_buy;
+                            bins_per_second = newestbps;
+                            int newestmoney = money - bps_can_buy_price;
+                            money = newestmoney;
+                            Console.Clear();
+                            continue;
+                        }
+
+                        if(bps_buy1 == "n") {
+                            Console.Clear();
+                            continue;
+                        }
+
+                        else {
+                            Console.WriteLine("Incorrect answer, use (y/n)");
+                            Thread.Sleep(570);
+                            Console.Clear();
+                            continue;
+                        }
+                    }
+
+                    if(shopInput1 == "2") {
+                        int amount = bins * price;
+                        Console.WriteLine("Are you sure you would like to trade your bins? (y/n)");
+                        string tradeInput1 = Console.ReadLine();
+
+                        if(tradeInput1 == "y") {
+                            int addedmoneyv2 = amount + money;
+                            money = addedmoneyv2;
+                            continue;
+                        }
+
+                        if(tradeInput == "n") {
+                            continue;
+                        }
+
+                        else {
+                            Console.WriteLine("Incorrect statement use (y/n)");
+                            Thread.Sleep(750);
+                            continue;
+                        }
+                    }
+
+                    if(shopInput1 == "3") {
+                        continue;
+                    }
+                }
                 
                 else {
                     goto Main;
                 }
             }
     } 
-}
+}  
